417045536431668f0c45bf13ea69e5d6bb50c78b
Admin <Admin@DESKTOP-40H717B>
2025-05-07 16:07:16 +0700

    first commit

---
+++ RERPAPI.Model/Common/SQLHelper.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Model.Common
+{
+    public static class SQLHelper<T> where T : class, new()
+    {
+        static string connectionString = Config.ConnectionString;
+        static int commandTimeout = 2000;
+        public static T ProcedureToModel(string procedureName, string[] paramName, object[] paramValue)
+        {
+            T model = new T();
+            SqlConnection mySqlConnection = new SqlConnection(connectionString);
+            SqlParameter sqlParam;
+            mySqlConnection.Open();
+
+            try
+            {
+                SqlCommand mySqlCommand = new SqlCommand(procedureName, mySqlConnection);
+                mySqlCommand.CommandType = CommandType.StoredProcedure;
+                if (paramName != null)
+                {
+                    for (int i = 0; i < paramName.Length; i++)
+                    {
+                        sqlParam = new SqlParameter(paramName[i], paramValue[i]);
+                        mySqlCommand.Parameters.Add(sqlParam);
+                    }
+                }
+                SqlDataReader reader = mySqlCommand.ExecuteReader();
+                model = reader.MapToSingle<T>();
+            }
+            catch (SqlException e)
+            {
+                throw new Exception(e.ToString());
+            }
+            finally
+            {
+                mySqlConnection.Close();
+            }
+
+            return model;
+        }
+        public static List<T> ProcedureToList(string procedureName, string[] paramName, object[] paramValue)
+        {
+            List<T> lst = new List<T>();
+            SqlConnection mySqlConnection = new SqlConnection(connectionString);
+            SqlParameter sqlParam;
+            mySqlConnection.Open();
+
+            try
+            {
+                SqlCommand mySqlCommand = new SqlCommand(procedureName, mySqlConnection);
+                mySqlCommand.CommandType = CommandType.StoredProcedure;
+                mySqlCommand.CommandTimeout = commandTimeout;
+                if (paramName != null)
+                {
+                    for (int i = 0; i < paramName.Length; i++)
+                    {
+                        sqlParam = new SqlParameter(paramName[i], paramValue[i]);
+                        mySqlCommand.Parameters.Add(sqlParam);
+                    }
+                }
+                SqlDataReader reader = mySqlCommand.ExecuteReader();
+                lst = reader.MapToList<T>();
+            }
+            catch (SqlException e)
+            {
+                throw new Exception(e.ToString());
+            }
+            finally
+            {
+                mySqlConnection.Close();
+            }
+
+            return lst;
+        }
+    }
+}
---
+++ RERPAPI.Repo/GenericRepo.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RERPAPI.IRepo;
+using RERPAPI.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Repo
+{
+    public class GenericRepo<T> : IGenericRepo<T> where T : class, new()
+    {
+        protected RTCContext db { get; set; }
+        protected DbSet<T> table = null;
+        public GenericRepo()
+        {
+            db = new RTCContext();
+            table = db.Set<T>();
+        }
+
+        public GenericRepo(RTCContext db)
+        {
+            this.db = db;
+            table = db.Set<T>();
+        }
+
+        public List<T> GetAll()
+        {
+            try
+            {
+                return table.ToList();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        public T GetByID(int id)
+        {
+            try
+            {
+                T model = table.Find(id) ?? new T();
+                return model;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        public int Create(T item)
+        {
+            try
+            {
+                table.Add(item);
+                return db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        public int CreateRange(List<T> items)
+        {
+            try
+            {
+                table.AddRange(items);
+                return db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        public int Update(T item)
+        {
+            try
+            {
+                table.Attach(item);
+                db.Entry(item).State = EntityState.Modified;
+                return db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        public int Delete(int id)
+        {
+            try
+            {
+                T model = table.Find(id) ?? new T();
+                table.Remove(model);
+                return db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        public async Task<int> CreateAsync(T item)
+        {
+            try
+            {
+                await table.AddAsync(item);
+                return db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        public async Task<int> UpdateAsync(T item)
+        {
+            try
+            {
+                table.Attach(item);
+                db.Entry(item).State = EntityState.Modified;
+                return await db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            try
+            {
+                T model = await table.FindAsync(id) ?? new T();
+                table.Remove(model);
+                return await db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        public async Task<int> CreateRangeAsync(List<T> items)
+        {
+            try
+            {
+                await table.AddRangeAsync(items);
+                return await db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        public int DeleteRange(List<T> items)
+        {
+
+            try
+            {
+                table.RemoveRange(items);
+                return db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        public async Task<int> DeleteRangeAsync(List<T> items)
+        {
+            try
+            {
+                table.RemoveRange(items);
+                return await db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+    }
+}
